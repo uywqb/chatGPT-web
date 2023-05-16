5a1fbcf2defd2b686025ee93d55c6d8ed9a36a02
uywqb <lei.sunlei@gmail.com>
2023-05-16 13:35:31 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@
 SECRET_KEY = "asghnjadfbtywyn"
 
 # openAi api key
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = ""
 
 # openAi 官方 api
 # URL = "https://api.openai.com/v1/chat/completions"
